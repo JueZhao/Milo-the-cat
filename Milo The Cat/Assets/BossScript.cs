31b6accf3552493b6eb448d1f22399c26b200b33
kmj4789 <81201559+kmj4789@users.noreply.github.com>
2021-04-30 18:34:28 +1200

    Boss Add
    Adding Boss level

---
+++ Milo The Cat/Assets/BossScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BossScript : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+
+        if (col.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+
+        if (col.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+    }
+}
