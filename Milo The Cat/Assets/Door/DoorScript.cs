01ff37f866110d8c5a547abf3b33336ca230477f
kmj4789 <81201559+kmj4789@users.noreply.github.com>
2021-04-30 17:12:56 +1200

    resubmitting current version
    oops

---
+++ Milo The Cat/Assets/Door/DoorScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class DoorScript : MonoBehaviour
+{
+
+    public int LeveltoLoad;
+    
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+
+        if (col.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+
+        if (col.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+    }
+
+
+
+}
