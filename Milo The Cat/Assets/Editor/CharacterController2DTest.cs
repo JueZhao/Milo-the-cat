02dbae00da597fe024ea075b566c5fa08236ed56
kmj4789 <81201559+kmj4789@users.noreply.github.com>
2021-04-30 20:43:01 +1200

    level touch ups

---
+++ Milo The Cat/Assets/Editor/CharacterController2DTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class CharacterController2DTest : MonoBehaviour
+{
+    [Test]
+    public void CharacterController2D_Test()
+    {
+
+    }
+}
