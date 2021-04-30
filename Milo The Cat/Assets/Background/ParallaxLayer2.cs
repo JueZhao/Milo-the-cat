09f78227937fbdfa326b068ea33f3855242c61d8
kmj4789 <81201559+kmj4789@users.noreply.github.com>
2021-04-30 16:56:28 +1200

    Many Changes
    Added bottom collider to reload level and added door to next level. created next level scene. Created tutorial screen.

---
+++ Milo The Cat/Assets/Background/ParallaxLayer2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxLayer2 : MonoBehaviour
+{
+    public Camera cam;
+    public Transform subject;
+
+    Vector2 startPosition;
+    float startZ;
+
+    Vector2 travel => (Vector2)cam.transform.position - startPosition;
+    Vector2 parallaxFactor;
+
+    public void start()
+    {
+
+        startPosition = transform.position;
+        startZ = transform.position.z;
+
+
+    }
+
+    public void Update()
+    {
+
+        transform.position = startPosition + travel;
+
+    }
+
+
+}
