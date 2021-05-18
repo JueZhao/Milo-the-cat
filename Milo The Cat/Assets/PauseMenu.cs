df82ee84d725f675ca3afe3d554ffcd36e76fc77
Den-git-tech <69407683+Den-git-tech@users.noreply.github.com>
2021-05-19 04:00:28 +1200

    PauseMenu working

--- Milo The Cat/Assets/PauseMenu.cs
+++ Milo The Cat/Assets/PauseMenu.cs
@@ -1,18 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenu;
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }else
+            {
+                Pause();
+            }
+        }  
         
     }
+    
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        }
+        public void Resume()
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+            GameIsPaused = false;
+        }
+
+        public void SoundMenu()
+        {
+            
+        }
+        public void LoadMenu()
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        public void QuitGame()
+        {
+            Debug.Log("GOODBYE...");
+            Application.Quit();
+        }
 }
